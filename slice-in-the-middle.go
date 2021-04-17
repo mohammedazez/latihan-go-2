8602b4f2f939aea607e7c5bbb192455d81a9e968
mohammedazez <muhamadaziz047@gmail.com>
2021-04-17 23:39:41 +0700

    slice in the middle is done

--- slice-in-the-middle.go
+++ slice-in-the-middle.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main(){
+	data := [5]string{"a", "i", "u", "e", "o"}
+
+	satu := data[len(data) / 2 | 0]
+	dua := data[len(data) / 3 | 0]
+	tiga := data[len(data) / 2 | 1]
+
+	values := []string{}
+
+
+	if len(data) % 2 == 0 {
+		values = append(values, satu)
+		values = append(values, dua)
+		result := strings.Join(values, ",")
+		toArray := strings.Fields(result)
+		fmt.Println(toArray)
+	} else {
+		values = append(values, satu)
+		values = append(values, dua)
+		values = append(values, tiga)
+		result := strings.Join(values, ",")
+		toArray := strings.Fields(result)
+		fmt.Println(toArray)
+	}
+}
