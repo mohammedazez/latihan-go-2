94d2a6d951bd5c956cffdd2b6c5985c466655abe
mohammedazez <muhamadaziz047@gmail.com>
2021-04-17 21:08:21 +0700

    sum-max-and-min.go is done

--- sum-max-and-min.go
+++ sum-max-and-min.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var number = [...]int{10, 120, 14, 50, 5}
+
+	smallesNumber := number[0]
+	biggestNumber := number[0]
+
+	for _, element := range number {
+		if element < smallesNumber {
+			smallesNumber = element
+		}
+
+		if element > biggestNumber {
+			biggestNumber = element
+		}
+	}
+
+	fmt.Println("Ini adalah array terkecil", smallesNumber)
+	fmt.Println("Ini adalah array terbesar", biggestNumber)
+
+	postiveSum := biggestNumber + smallesNumber
+	negativeSum := biggestNumber - smallesNumber
+	result := postiveSum + negativeSum
+
+	fmt.Println(postiveSum)
+	fmt.Println(negativeSum)
+	fmt.Println(result)
+}
